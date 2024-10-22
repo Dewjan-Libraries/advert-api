0a5ef0edf380d9bda6f4c5253f5aec6f6918b743
ownyanteh <ownyanteh@gmail.com>
2024-10-22 14:25:58 +0000

    upload worked on

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,32 @@
 import { expressjwt } from "express-jwt";
 import { VendorModel } from "../models/vendors.js";
+import { permissions } from "../utils/rbac.js";
 
 export const isAuthenticated = expressjwt({
     secret: process.env.JWT_PRIVATE_KEY,
     algorithms: ["HS256"]
 });
+
+export const hasPermission = (action) => {
+    return async (req, res, next) => {
+        try {
+            // find user from database
+            const vendor = await VendorModel.findById(req.auth.id);
+            // use the user role to find their permissio
+            const permission = permissions.find(value => value.role === vendor.role);
+            if (!permission) {
+                return res.status(403).json('No permission found');
+            }
+            // check if permission actions invlude action
+            if (permission.actions.includes(action)) {
+                next();
+            } else {
+                res.status(403).json('Action not allowed')
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
  
