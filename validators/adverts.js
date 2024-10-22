652c82d93470ea633a2ead1c34096a6d714c4b83
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-22 13:44:48 +0000

    adding validations

--- validators/adverts.js
+++ validators/adverts.js
@@ -1,12 +1,22 @@
 import Joi from "joi";
 
-export const addAdvertValidator =Joi.object({
-    title:Joi.string().required,
-    category:Joi.string().required,
-    condition:Joi.string().required,
-    brandName:Joi.string().required,
-    price:Joi.string().required,
-    image:Joi.string().required,
-    vendor:Joi.string().required,
-    desription:Joi.string().required,
+
+export const addAdvertValidator = Joi.object({
+  title: Joi.string().required,
+  category: Joi.string().required,
+  condition: Joi.string().required,
+  brandName: Joi.string().required,
+  price: Joi.string().required,
+  image: Joi.string().required,
+  description: Joi.string().required,
+});
+
+export const updateAdvertValidator = Joi.object({
+  title: Joi.string(),
+  category: Joi.string(),
+  condition: Joi.string(),
+  brandName: Joi.string(),
+  price: Joi.string(),
+  image: Joi.string(),
+  description: Joi.string(),
 });
