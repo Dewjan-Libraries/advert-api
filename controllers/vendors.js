64968f966a48bafff82c02e50e1afba07a622761
Jacqueline Offei <offeijacqueline@gmail.com>
2024-10-24 13:48:53 +0000

    adding email message

--- controllers/vendors.js
+++ controllers/vendors.js
@@ -1,99 +1,103 @@
 import { VendorModel } from "../models/vendors.js";
-import pkg from "bcryptjs"
+import pkg from "bcryptjs";
 const { hashSync, compareSync } = pkg;
-import jwt from 'jsonwebtoken'
-import { loginVendorValidator, registerVendorValidator, updateVendorValidator } from "../validators/vendors.js";
-
+import jwt from "jsonwebtoken";
+import {
+  loginVendorValidator,
+  registerVendorValidator,
+  updateVendorValidator,
+} from "../validators/vendors.js";
+import { mailTransporter } from "../utils/mail.js";
 
 // register vendor controller
 export const registerVendor = async (req, res, next) => {
-    try {
-        // validate vendor input
-        const { error, value } = registerVendorValidator.validate(req.body);
-        if (error) {
-            return res.status(422).json(error);
-        }
-        // check if vendor exist
-        const vendor = await VendorModel.findOne({ email: value.email });
-        if (vendor) {
-            return res.status(409).json("User already exist!")
-        }
-        // hash their password
-        const hashPassword = hashSync(value.password, 10);
-        // save user to database
-        await VendorModel.create({
-            ...value,
-            password: hashPassword
-        })
-        // send confirmation email
-        // respond to request
-        res.json('User Registered Successfully')
-    } catch (error) {
-        next(error)
+  try {
+    // validate vendor input
+    const { error, value } = registerVendorValidator.validate(req.body);
+    if (error) {
+      return res.status(422).json(error);
     }
-
-
+    // check if vendor exist
+    const vendor = await VendorModel.findOne({ email: value.email });
+    if (vendor) {
+      return res.status(409).json("User already exist!");
+    }
+    // hash their password
+    const hashPassword = hashSync(value.password, 10);
+    // save user to database
+    await VendorModel.create({
+      ...value,
+      password: hashPassword,
+    });
+    await mailTransporter.sendMail({
+      to: value.email,
+      subject: "User Registered",
+      text: "Account registered! Postiize...post with ease",
+    });
+    // send confirmation email
+    // respond to request
+    res.json("User Registered Successfully");
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const loginVendor = async (req, res, next) => {
-    try {
-        // validate vendor input
-        const { error, value } = loginVendorValidator.validate(req.body);
-        if (error) {
-            return res.status(422).json(error);
-        }
-        // find one vendor with identifier
-        const vendor = await VendorModel.findOne({ email: value.email });
-        if (!vendor) {
-            return res.status(404).json('Invalid Credentials');
-        }
-        // compare passwords
-        const correctPassword = compareSync(value.password, vendor.password);
-        if (!correctPassword) {
-            return res.status(401).json('Invalid Credentials')
-        }
-        // sign a token for vendor
-        const token = jwt.sign(
-            { id: vendor.id },
-            process.env.JWT_PRIVATE_KEY,
-            { expiresIn: '24h' }
-        );
-        // respond to request
-        res.status(200).json({
-            message: "User Loggedin Successfully",
-            accessToken: token
-        })
-    } catch (error) {
-        next(error)
+  try {
+    // validate vendor input
+    const { error, value } = loginVendorValidator.validate(req.body);
+    if (error) {
+      return res.status(422).json(error);
     }
-
-}
+    // find one vendor with identifier
+    const vendor = await VendorModel.findOne({ email: value.email });
+    if (!vendor) {
+      return res.status(404).json("Invalid Credentials");
+    }
+    // compare passwords
+    const correctPassword = compareSync(value.password, vendor.password);
+    if (!correctPassword) {
+      return res.status(401).json("Invalid Credentials");
+    }
+    // sign a token for vendor
+    const token = jwt.sign({ id: vendor.id }, process.env.JWT_PRIVATE_KEY, {
+      expiresIn: "24h",
+    });
+    // respond to request
+    res.status(200).json({
+      message: "User Loggedin Successfully",
+      accessToken: token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getProfile = async (req, res, next) => {
-    try {
-        // find authenticated user from database
-        const vendor = await VendorModel
-            .findById(req.auth.id)
-            .select({ password: false })
-            // respond to request
-            res.json(vendor);
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    // find authenticated user from database
+    const vendor = await VendorModel.findById(req.auth.id).select({
+      password: false,
+    });
+    // respond to request
+    res.json(vendor);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const logoutVendor = (req, res, next) => {
-    res.json('Vendor successfully logged out')
+  res.json("Vendor successfully logged out");
 };
 
 export const updateProfile = (req, res, next) => {
-    try {
-        const {error, value} = updateVendorValidator.validate(req.body);
-        if(error) {
-            return res.status(422).json(error)
-        }
-        res.json("Profile Updated Successfully")
-    } catch (error) {
-        next(error)
+  try {
+    const { error, value } = updateVendorValidator.validate(req.body);
+    if (error) {
+      return res.status(422).json(error);
     }
-}
+    res.json("Profile Updated Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
